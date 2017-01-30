f06c1c37f0447d6d1f46f251e48e92e7d1df355d
Victory Garuba <Victory Garuba>
2017-01-31 00:18:05 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,64 @@
+<header class="pink lighten-1 ">
+    <div class="row">
+        <nav class="navbar-extended">
+            <div class="nav-wrapper row">
+
+                <a href="" class="valign-wrapper col m12 l5 flow-text">
+                    <h1 class="brand-logo valign left-align">Hospital Self-Service Portal</h1>
+                </a>
+                <p class="valign right-align col m12 l4">
+                        You are logged in as
+                    </p>
+            </div>
+            <div class="nav-content">
+                <a href="#" data-activates="mobile-nav" class="button-collapse ">
+                    <i class="material-icons">menu</i>
+                </a>
+                <ul class="hide-on-med-and-down left">
+                    <li>
+                        <a href="#"><i class="material-icons left">home</i>Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">thumb_up</i>Authorization</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">today</i>Appointment</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">person</i>Client/Enrollee Details</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">local_hospital</i>Treatment</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">sms_failed</i>Feedback</a>
+                    </li>
+
+                </ul>
+                <a href="" class="waves-effect waves-light btn  right logstatus">Log in</a>
+
+                <ul class="side-nav" id="mobile-nav">
+                    <li>
+                        <a href="#"><i class="material-icons left">home</i>Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">thumb_up</i>Authorization</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">today</i>Appointment</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">person</i>Client/Enrollee Details</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">local_hospital</i>Treatment</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons left">sms_failed</i>Feedback</a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/materialize.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php require('header.php');?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
